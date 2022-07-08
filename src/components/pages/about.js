74796ce8367edb8bfbefaf19b43f81e572814939
Ethan Wilson <ethan.w450@nebo.local>
2022-07-08 11:51:12 -0600

    Second commit 7/8/2022

---
+++ src/components/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function() {
+    return (
+        <div>
+            About
+            <h1>Ethan Wilson</h1>
+            <h2>The almighty Fishlord</h2>
+            <h3>Goetian King</h3>
+        </div>
+        
+    );
+}
